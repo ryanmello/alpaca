d0d1f4dc086802c4d2ea4661015f198cc1e183df
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-06-14 07:35:11 -0700

    application setup

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AuthForm = () => {
+  return (
+    <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+        <div className='bg-white px-4 py-8 shadow rounded-lg sm:px-10'>
+            Auth Form
+        </div>
+    </div>
+  )
+}
+
+export default AuthForm
---
+++ app/(site)/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import AuthForm from "./components/AuthForm"
+
+const Auth = () => {
+  return (
+    <div className="h-full flex flex-col justify-center items-center bg-gray-100">
+      <h2 className="text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+      <AuthForm />
+    </div>
+  )
+}
+
+export default Auth
+
